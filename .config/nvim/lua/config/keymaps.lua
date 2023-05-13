6dc15d741c45129bcf8d00f4ec217e9b4b726b45
linhng98 <linh1612340@gmail.com>
2023-05-13 23:52:23 +0700

    feat: migrate nvim to lua config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,35 @@
+-- copy selected visual block text area to clipboard
+vim.keymap.set('v', '<C-c>', '"+y')
+
+-- remove highlight after search
+vim.keymap.set('n', '<C-h>', ':noh<CR>')
+
+-- switch tab
+vim.keymap.set('n', '<Tab>', ':bnext<CR>')
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>')
+
+-- save and quit window
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-q>', ':wq<CR>')
+
+-- resize window
+vim.keymap.set('n', '<C-M-h>', ':vertical resize -5<CR>')
+vim.keymap.set('n', '<C-M-l>', ':vertical resize +5<CR>')
+vim.keymap.set('n', '<C-M-j>', ':horizontal resize -5<CR>')
+vim.keymap.set('n', '<C-M-k>', ':horizontal resize +5<CR>')
+
+-- move between window
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-Left>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-Right>', ':wincmd l<CR>')
+vim.keymap.set('n', '<C-Down>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-Up>', ':wincmd k<CR>')
+
+-- nvim tree toggle
+vim.keymap.set('n', '<Leader>t', ':NvimTreeFocus<CR>')
+vim.keymap.set('n', '<Leader>pv', vim.cmd.Ex)
+
+
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,23 @@
+vim.cmd [[syntax enable]]
+vim.cmd [[colorscheme dracula]]
+
+-- Auto jump to previous working line
+vim.cmd([[
+  if has("autocmd")
+    au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+  endif
+]])
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.laststatus = 2
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.smarttab = true
+vim.opt.smartindent= true
+vim.opt.expandtab= true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
